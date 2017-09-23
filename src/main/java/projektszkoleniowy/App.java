66324002c5de6c431f2ce509b45473a412c6a1e7
Ewa Jablonska <mizancewa@gmail.com>
2017-09-23 17:48:50 +0200

    first commit

---
+++ src/main/java/projektszkoleniowy/App.java
@@ -0,0 +1,13 @@
+package projektszkoleniowy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
